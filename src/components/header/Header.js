24a319a0ee3439feee835e0084cc9d591c62c8b1
chandanhm1999 <109410990+chandanhm1999@users.noreply.github.com>
2023-04-17 17:24:52 +0530

    initial

---
+++ src/components/header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import { NavLink } from 'react-router-dom'
+import logo from "../../assets/img1/logo.jpg"
+import "./header.css"
+
+const Header = () => {
+    return (
+        <>
+            <Navbar bg="dark" expand="lg" variant="dark" >
+                <NavLink to="/" className="p-0 text-decoration-none text-light mx-2" >
+                    <img className='logo_png' src={logo} alt='/' />
+                </NavLink>
+                <Container className='items_1'>
+                <NavLink to="/" className=" text-decoration-none text-light mx-5">Home</NavLink>
+                    <NavLink to="/" className=" text-decoration-none text-light">Sign Up</NavLink>
+                    <Nav className="me-auto">
+                        <NavLink to="/" className="text-decoration-none text-light mx-5">Home</NavLink>
+                        <NavLink to="/" className="text-decoration-none text-light mx-2">Features</NavLink>
+                    </Nav>
+                    
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default Header
